efb8fdb13daebc78b60f710e53861af99dc55843
serebias69 <serebiad11169@gmail.com>
2018-08-31 21:01:35 -0300

    meu app

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+     <link rel="stylesheet" href="css/main.css">
+   <title>Inicio</title>
+</head>
+
+<body>
+    <header> <!-- cabeçalho -->
+                <aside> <span class="laranja">Login test</span> </aside>
+                <nav>
+                    <a class="ativo" href="index.html">Inicio</a>
+                    |
+                    <a href=" ">Entrar</a>
+                    |
+                    <a href=" ">Novo usuario</a>
+                </nav>
+    </header>
+
+        <div id="conteudo">
+            <p>Sejam Bem Vindos!!!</p>
+        </div>  <!--termina conteudo-->
+
+        <footer>
+             <p>Android SDK</p>
+        </footer>
+
+</body>
+</html>
